abcfa0d7a7c6b21ad389e7d4d2745ca1c5211c16
Shen <s_lin97@hotmail.com>
2019-10-17 01:07:30 +0200

    Dijkstra Agent

---
+++ Unity/Winjerman/Assets/Scenes/Scripts/DijkstraAgent.cs
@@ -0,0 +1,30 @@
+﻿/*using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+using Unity.Collections;
+
+using Rules = WindjermanGameStateRules;
+
+
+public class DijkstraAgent : IAgent
+{
+    private int PlayerID;
+    public DijkstraAgent(int PlayerID)
+    {
+        this.PlayerID = PlayerID;
+    }
+
+    public int Act(ref WindjermanGameState gs, int[] availableActions)
+    {
+        var agent = new DijkstraAgent();
+
+        
+        
+    }
+
+
+
+
+}
+*/
